09b2298301b1e574f98bac929974c7092b5b265d
Nurhaliza Hassan <nurhalizahassan@Nurhalizas-MBP.lan>
2021-10-30 19:27:26 -0700

    tidied each of 3 datasets - need to finish one more component and combine all datasets

---
+++ wvr-dw.R
@@ -0,0 +1,48 @@
+# environment used: rstudio - language: r
+# libraries needed
+library(tidyverse) 
+library(ggplot2)
+
+# saving all datasets to corresponding data names 
+data1 <- read_csv("https://raw.githubusercontent.com/govex/COVID-19/master/data_tables/vaccine_data/global_data/time_series_covid19_vaccine_doses_admin_global.csv")
+data2 <- read_csv("/Users/nurhalizahassan/Desktop/Project1/API_NY.GDP.MKTP.CD_DS2_en_csv_v2_3011433.csv")
+data3 <- read_csv("/Users/nurhalizahassan/Desktop/Project1/demographics.csv")
+
+# data 1 - time_series_covid19_vaccine_doses_admin_global.csv
+# - tidying/wrangling - 
+# note: the following commands are by parts
+revised_data1 <- data1 %>% select(-'UID', -'iso2', -'code3', -'FIPS', -'Admin2', -'Province_State', -'Lat', -'Long_', -'Combined_Key')
+revised_data1 <- revised_data1 %>% pivot_longer(-c(iso3, Country_Region, Population), names_to = "Date", values_to = "Shots", values_drop_na = TRUE) %>% filter(Shots != 0) 
+revised_data1 <- revised_data1 %>% mutate(Vaccination_Rate = Shots / Population) 
+# note: final comprehensive command
+revised_data1 <- data1 %>% select(-'UID', -'iso2', -'code3', -'FIPS', -'Admin2', -'Province_State', -'Lat', -'Long_', -'Combined_Key') %>% pivot_longer(-c(iso3, Country_Region, Population), names_to = "Date", values_to = "Shots", values_drop_na = TRUE) %>% filter(Shots != 0) %>% mutate(Vaccination_Rate = Shots / Population) 
+
+# data 2 - API_NY.GDP.MKTP.CD_DS2_en_csv_v2_3011433.csv
+# - tidying/wrangling -
+# note: the following commands are by parts
+revised_data2 <- data2 %>% select(-'Country Code', -'Indicator Code', -'Indicator Name')
+revised_data2 <- data2 %>% pivot_longer(-'Country Name', names_to = "Year", values_to = "GDP", values_drop_na = TRUE) %>% filter(Year == 2005 & GDP != 0)
+# note: final comprehensive command
+revised_data2 <- data2 %>% select(-'Country Code', -'Indicator Code', -'Indicator Name') %>% pivot_longer(-'Country Name', names_to = "Year", values_to = "GDP", values_drop_na = TRUE) %>% filter(Year == 2005 & GDP != 0)
+
+# data 3 - demographics.csv
+# - tidying/wrangling -
+# note: the following commands are by parts
+# must first combine male and female population numbers together (ignore sex-related differences)
+# SP.POP.65.UP.IN
+data3 %>% select(-`Series Name`) %>% pivot_wider(names_from = `Series Code`, values_from = `YR2015`) %>% mutate(SP.POP.65UP.IN = SP.POP.65UP.FE.IN + SP.POP.65UP.MA.IN) 
+# SP.POP.80.UP.IN
+data3 %>% select(-`Series Name`) %>% pivot_wider(names_from = `Series Code`, values_from = `YR2015`) %>% mutate(SP.POP.80UP.IN = SP.POP.80UP.FE + SP.POP.80UP.MA) 
+# SP.POP.1564.IN
+data3 %>% select(-`Series Name`) %>% pivot_wider(names_from = `Series Code`, values_from = `YR2015`) %>% mutate(SP.POP.1564.IN = SP.POP.1564.FE.IN + SP.POP.1564.MA.IN) 
+# SP.POP.0014.IN
+data3 %>% select(-`Series Name`) %>% pivot_wider(names_from = `Series Code`, values_from = `YR2015`) %>% mutate(SP.POP.0014.IN = SP.POP.0014.FE.IN + SP.POP.0014.MA.IN) 
+# SP.DYN.AMRT
+data3 %>% select(-`Series Name`) %>% pivot_wider(names_from = `Series Code`, values_from = `YR2015`) %>% mutate(SP.DYN.AMRT = SP.DYN.AMRT.FE + SP.DYN.AMRT.MA) 
+# note: comprehensive command for male and female population integrated combination 
+data3 %>% select(-`Series Name`) %>% pivot_wider(names_from = `Series Code`, values_from = `YR2015`) %>% mutate(SP.POP.65UP.IN = SP.POP.65UP.FE.IN + SP.POP.65UP.MA.IN) %>% mutate(SP.POP.65UP.IN = SP.POP.65UP.FE.IN + SP.POP.65UP.MA.IN) %>% mutate(SP.POP.80UP.IN = SP.POP.80UP.FE + SP.POP.80UP.MA) %>% mutate(SP.POP.1564.IN = SP.POP.1564.FE.IN + SP.POP.1564.MA.IN) %>% mutate(SP.POP.0014.IN = SP.POP.0014.FE.IN + SP.POP.0014.MA.IN) %>% mutate(SP.DYN.AMRT = SP.DYN.AMRT.FE + SP.DYN.AMRT.MA) %>% select(-c(SP.POP.80UP.FE,SP.POP.80UP.MA,SP.POP.1564.MA.IN,SP.POP.1564.FE.IN,SP.POP.0014.MA.IN,SP.POP.0014.FE.IN,SP.DYN.AMRT.FE,SP.DYN.AMRT.MA,SP.POP.TOTL.FE.IN,SP.POP.TOTL.MA.IN,SP.POP.65UP.FE.IN,SP.POP.65UP.MA.IN))
+# note: final comprehensive commands
+revised_data3 <- data3 %>% select(-`Series Name`) %>% pivot_wider(names_from = `Series Code`, values_from = `YR2015`) %>% mutate(SP.POP.65UP.IN = SP.POP.65UP.FE.IN + SP.POP.65UP.MA.IN) %>% mutate(SP.POP.65UP.IN = SP.POP.65UP.FE.IN + SP.POP.65UP.MA.IN) %>% mutate(SP.POP.80UP.IN = SP.POP.80UP.FE + SP.POP.80UP.MA) %>% mutate(SP.POP.1564.IN = SP.POP.1564.FE.IN + SP.POP.1564.MA.IN) %>% mutate(SP.POP.0014.IN = SP.POP.0014.FE.IN + SP.POP.0014.MA.IN) %>% mutate(SP.DYN.AMRT = SP.DYN.AMRT.FE + SP.DYN.AMRT.MA) %>% select(-c(SP.POP.80UP.FE,SP.POP.80UP.MA,SP.POP.1564.MA.IN,SP.POP.1564.FE.IN,SP.POP.0014.MA.IN,SP.POP.0014.FE.IN,SP.DYN.AMRT.FE,SP.DYN.AMRT.MA,SP.POP.TOTL.FE.IN,SP.POP.TOTL.MA.IN,SP.POP.65UP.FE.IN,SP.POP.65UP.MA.IN)) 
+revised_data3 <- revised_data3 %>% select('Country Name', 'SP.DYN.LE00.IN', 'SP.URB.TOTL')
+
+# combining all datasets
